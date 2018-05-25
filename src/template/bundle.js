3855cb349195717fd118b20063de5c2bba18d9f1
Robin Bron <robin@finwo.nl>
2018-05-25 14:06:52 +0200

    Forgot to add event plugin to the bundle

--- src/template/bundle.js
+++ src/template/bundle.js
@@ -14,6 +14,7 @@
 
   // ---[ module plugin/toggleClass.js ]---(fw);
   // ---[ module plugin/animate.js ]---(fw);
+  // ---[ module plugin/events.js ]---(fw);
 
 })(function() {
   var module = {exports : undefined};
